c9ecbdae2fdbe9d6ef59020996b3b82e5805df52
Shivam Gera <shivam.g@tapchief.com>
2020-07-20 07:23:33 +0530

    more fixes

--- roiim/payment/admin.py
+++ roiim/payment/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from .models import Payment
 # Register your models here.
-class PAymentAdmin(admin.ModelAdmin):
+class PaymentAdmin(admin.ModelAdmin):
     pass
 
 
