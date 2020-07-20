bc0eea8192ab85c6d9928db8ab8808df7dc1ad52
Shivam Gera <shivam.g@tapchief.com>
2020-07-20 07:27:25 +0530

    wsgi fix

--- roiim/settings.py
+++ roiim/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,7 +137,7 @@ USE_L10N = True
 
 
 USE_TZ = True
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
--- roiim/wsgi.py
+++ roiim/wsgi.py
@@ -10,10 +10,8 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'roiim.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root='roiim/static/')
-application.add_files('templates/static/', prefix='more-files/')
